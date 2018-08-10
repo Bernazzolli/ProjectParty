a6b77f6e455222b03f3a9dfbbc88b2048d6258e2
Bernazzolli <34899586+Bernazzolli@users.noreply.github.com>
2018-08-09 22:14:26 -0300

    Update MainActivity.java

--- src/main/java/com/example/cesarbernasoli/party2tonight3/MainActivity.java
+++ src/main/java/com/example/cesarbernasoli/party2tonight3/MainActivity.java
@@ -1,25 +1,27 @@
-package com.example.cesarbernasoli.party2tonight3;
-
+package com.example.cesarbernasoli.projectparty;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.support.design.widget.TabLayout;
-
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-
 import android.os.Bundle;
-
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-
 
 public class MainActivity extends AppCompatActivity {
     Button btnReader,btnGenerator;
@@ -35,12 +37,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnReader = (Button) findViewById(R.id.btnReader);
-        btnGenerator = (Button) findViewById(R.id.btnGenerator);
-
-
-
-
         Log.d(TAG, "onCreate: Starting.");
 
         mSectionsPageAdapter = new SectionpageAdapter(getSupportFragmentManager());
@@ -52,22 +48,13 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-
-
-
-
-
-
-
-
-
     }
 
     private void setupViewPager(ViewPager viewPager) {
         SectionpageAdapter adapter = new SectionpageAdapter(getSupportFragmentManager());
-        adapter.addFragment(new tab1Fragment(), "Home");
-        adapter.addFragment(new tab2Fragment(), "Chat");
-        adapter.addFragment(new tab3Fragment(), "QR Code");
+        adapter.addFragment(new Home(), "Home");
+        adapter.addFragment(new Chat(), "Chat");
+        adapter.addFragment(new QRCode(), "QR Code");
         viewPager.setAdapter(adapter);
     }
 
