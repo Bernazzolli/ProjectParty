816bcf33ef8ab85eea8914eaa6d101bd4db90c8c
Bernazzolli <34899586+Bernazzolli@users.noreply.github.com>
2018-08-09 22:15:41 -0300

    Home.java

--- src/main/java/com/example/cesarbernasoli/party2tonight3/tab1Fragment.java
+++ src/main/java/com/example/cesarbernasoli/party2tonight3/tab1Fragment.java
@@ -1,21 +1,25 @@
-package com.example.cesarbernasoli.party2tonight3;
+package com.example.cesarbernasoli.projectparty;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class tab1Fragment extends Fragment {
-    private static final String TAG = "Tab1Fragment";
+public class Home extends Fragment {
+
+    private static final String TAG = "HOME";
 
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
-    {
-        View view = inflater.inflate(R.layout.tab1_fragment,container,false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragmet_home,container,false);
+
         return view;
     }
 }
