87ad45443c126dd2e8fde12fe2e469a5886494fb
Bernazzolli <34899586+Bernazzolli@users.noreply.github.com>
2018-08-09 22:14:03 -0300

    Update QrCode.java

--- src/main/java/com/example/cesarbernasoli/party2tonight3/QrCode.java
+++ src/main/java/com/example/cesarbernasoli/party2tonight3/QrCode.java
@@ -1,32 +1,45 @@
-package com.example.cesarbernasoli.party2tonight3;
+package com.example.cesarbernasoli.projectparty;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
-public class QrCode  extends AppCompatActivity {
-    Button btnReader,btnGenerator;
+public class QRCode extends Fragment {
 
+    private static final String TAG = "QR CODE";
+
+    private static final int QR_SCANNER_REQUEST_CODE = 312;
+    private Button btnReader,btnGenerator;
+    final Activity activity = getActivity();
+
+
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.qrcode_activity);
-        btnReader = (Button) findViewById(R.id.btnReader);
-        btnGenerator = (Button) findViewById(R.id.btnGenerator);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        final Activity activity = this;
+        View view = inflater.inflate(R.layout.fragment_qrcode,container,false);
+        btnReader = (Button) view.findViewById(R.id.btnReader);
+        btnGenerator = (Button) view.findViewById(R.id.btnGenerator);
 
         btnReader.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                IntentIntegrator integrator = new IntentIntegrator(activity);
+            public void onClick(View view) {
+
+               // alert("Ainda em desenvolvimento");
+                IntentIntegrator integrator = new IntentIntegrator(getActivity());
                 integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
                 integrator.setPrompt("Camera Scan");
                 integrator.setCameraId(0);
@@ -36,30 +49,41 @@ public class QrCode  extends AppCompatActivity {
 
         btnGenerator.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                alert("Ainda esta em desenvolvimento");
+            public void onClick(View view) {
+                // scan();
+                alert("Ainda em desenvolvimento");
             }
         });
+
+
+        return view;
     }
 
-
-
-
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
-        if(result != null){
-            if (result.getContents() !=  null){
-                alert(result.getContents());
-            }else{
-                alert("Scan cancelado");
-            }
-        }else{
-            super.onActivityResult(requestCode, resultCode, data);
+
+        if(result != null)
+        {
+            alert("result != null");
+         if(result.getContents() != null)
+         {
+             alert("result.getCOntent() != null");
+         }
+         else
+         {
+             alert("Scan Cancelado");
+         }
+        }
+        else
+        {
+            alert("ultimo else ");
         }
     }
 
-    private void alert(String msg){
-        Toast.makeText(getApplicationContext(),msg, Toast.LENGTH_LONG).show();
+    public void alert(String mensagem) {
+
+        Toast.makeText(getActivity().getApplicationContext(),mensagem,Toast.LENGTH_LONG).show();
+
     }
 }
